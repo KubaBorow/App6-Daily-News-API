03358509566717f9e88ee8860fa38b0fd37a332a
KubaBorow <jakubrcam@gmail.com>
2024-09-26 21:50:29 +0200

    Send an email with news titles and descriptions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "de0dfb2627464607a5b965fdb2bceded"
 url = 'https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=de0dfb2627464607a5b965fdb2bceded'
@@ -10,6 +11,11 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        body = body + article['title'] + "\n" + str(article['description']) + 2* "\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
+
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "jakubrcam@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "netflixherberta@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)

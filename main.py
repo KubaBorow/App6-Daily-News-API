ef4ce6c4922ee64ce038092ae963694665850cc4
KubaBorow <jakubrcam@gmail.com>
2024-09-26 20:58:33 +0200

    Access the news titles and descriptions

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+api_key = "de0dfb2627464607a5b965fdb2bceded"
+url = 'https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=de0dfb2627464607a5b965fdb2bceded'
+
+# Make request
+request = requests.get(url)
+
+# Get dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content['articles']:
+    print(article['title'])
